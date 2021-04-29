2127c8aaff7f3ea66e969c98aaad7381f3128502
francois hamy <francois.hamy-ics.france@ibm.com>
2021-04-29 19:37:15 +0200

    Fix typo in script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Hello World.', 'Share you code', 'Made with Love.'], 'text',['lime','lime','lime']);
+consoleText(['Hello World.', 'Share your code', 'Made with Love.'], 'text',['lime','lime','lime']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
